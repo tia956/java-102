c5cace4ea8a973817144a6f01d0a5aa419c5fab5
unknown <tiatieno956@gmail.com>
2023-11-17 09:50:09 +0300

    second commit

---
+++ Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    public static void main(String[] args) {
+        System.out.println("Title = Great Minds");
+        System.out.println("Author = John Smith");
+        System.out.println("Publisher = Microsoft");
+        System.out.println("Year = 1998");
+        System.out.println("Year Published = 2019");
+        System.out.println("ISBN = 0-061-964360");
+    }
+}
